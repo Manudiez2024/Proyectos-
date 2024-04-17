18b8fe34faad3fbea3956627928d52903d266854
alumno <alumno@Instituto.local>
2024-04-17 07:50:28 -0300

    new files

---
+++ Practica.js
@@ -0,0 +1,33 @@
+/*let ingresoFamiliar = prompt('ingrese dinero estimado que gane')
+
+if (ingresoFamiliar >= 1500000){
+    alert('perteneces a clase alta')
+} else if(ingresoFamiliar < 880000)  {
+alert('perteneces a clase baja')
+} else {
+    alert('perteneces clase media')
+}
+*/
+
+
+
+
+
+
+
+//Funcion de Suma
+function suma(n1, n2){
+    return n1 + n2
+}
+// Funcion de Resta
+function resta(n1, n2){
+  return n1 - n2
+}
+//Funcion de Division
+function division(n1, n2){
+     return n1/n2
+}
+// Funcion de potencia al Cubo
+function alcubo(n1){
+    return n1 * n1 * n1
+}
